229eb392edbea821a7bd48ee9ec37132ce4d9508
saravanasenthil <rlsaravanalogesh@gmail.com>
2024-05-09 12:52:31 +0530

    updated files

--- hide and show a element in layout using js/script.js
+++ hide and show a element in layout using js/script.js
@@ -1,9 +1,29 @@
-function toggleElement() {
-    var element = document.getElementById("elementToToggle");
+function hideShow() {
+    var element = document.getElementById("elementHide");
+    
     if (element.style.display === "none") {
       element.style.display = "block";
+      element.style = "margin-left : 0px"
+      // element.textContent = "Show Less";
+      // element.classList.add("show-less");
+      
+      
     } else {
       element.style.display = "none";
+      // element.textContent = "Show more";
+      // element.classList.add("show-more");
+      
     }
+    
   }
   
+
+  
+
+
+
+  
+
+
+ 
+      
